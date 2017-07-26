156d88197558bcf1fcedfbebaf24b8874120b9a9
honwen.chan <honwen.chan@gmail.com>
2017-07-26 18:52:49 +0800

    add LuCI-log

--- files/luci/controller/koolproxy.lua
+++ files/luci/controller/koolproxy.lua
@@ -1,7 +1,23 @@
+-- Copyright (C) 2016 chenhw2 <https://github.com/chenhw2>
+-- Licensed to the public under the MIT License.
+
 module("luci.controller.koolproxy", package.seeall)
 
 function index()
-  entry(
-    {"admin", "services", "koolproxy"},
-    cbi("koolproxy"), _("KoolProxy"), 55)
+	entry({"admin", "services", "koolproxy"},
+		alias("admin", "services", "koolproxy", "general"),
+		_("KoolProxy"), 50)
+
+	entry({"admin", "services", "koolproxy", "general"},
+		cbi("koolproxy"), _("General Settings"), 20).leaf = true
+
+	entry({"admin", "services", "koolproxy", "log"},
+		call("action_log"), _("System Log"), 30).leaf = true
+end
+
+function action_log()
+	local fs = require "nixio.fs"
+	local conffile = "/var/log/koolproxy_watchdog.log"
+	local watchdog = fs.readfile(conffile) or ""
+	luci.template.render("admin_status/syslog", {syslog=watchdog})
 end
--- files/luci/model/cbi/koolproxy.lua
+++ files/luci/model/cbi/koolproxy.lua
@@ -1,3 +1,6 @@
+-- Copyright (C) 2016 chenhw2 <https://github.com/chenhw2>
+-- Licensed to the public under the MIT License.
+
 local m, s, o
 
 if luci.sys.call("pgrep koolproxy >/dev/null") == 0 then
@@ -7,10 +10,10 @@ else
 end
 
 s = m:section(TypedSection, "general", translate("General Setting"))
-s.anonymous   = true
+s.anonymous = true
 
 o = s:option(Flag, "enable", translate("Enable"))
-o.rmempty     = false
+o.rmempty = false
 
 o = s:option(Value, "startup_delay", translate("Startup Delay"))
 o:value(0, translate("Not enabled"))
@@ -18,8 +21,8 @@ for _, v in ipairs({5, 10, 15, 25, 40, 60, 120}) do
 	o:value(v, translate("%u seconds") %{v})
 end
 o.datatype = "uinteger"
-o.default = 0
-o.rmempty = false
+o.default  = 0
+o.rmempty  = false
 
 o = s:option(DynamicList, "lan_bp_list", translate("ByPass LAN"), translate("e.g. 192.168.1.100"))
 luci.ip.neighbors({family = 4}, function(neighbor)
